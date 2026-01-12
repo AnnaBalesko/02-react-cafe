ca666c3bf3d0eeaa763a591b2ed600da0ed54ec4
Anna Balesko <babasheldona@icloud.com>
2026-01-12 23:53:13 +0100

    add componets

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,19 @@
+import type { VoteType } from "../../types/votes";
+import css from "./VoteOptions.module.css";
+
+interface VoteOptionsProps{
+    onVote: (type: VoteType) => void;
+    onReset: () => void;
+    
+}
+
+export default function VoteOptions({onVote,onReset}:VoteOptionsProps){
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={()=>onVote('good')}>Good</button>
+      <button className={css.button} onClick={()=>onVote('neutral')}>Neutral</button>
+      <button className={css.button} onClick={()=>onVote('bad')}>Bad</button>
+      <button className={`${css.button} ${css.reset}`} onClick={()=>onReset()}>Reset</button>
+    </div>
+  );
+}
